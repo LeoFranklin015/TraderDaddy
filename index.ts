e6cc36c325b83ed849e35c85c7d98c7503de3ee8
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-04-14 21:33:22 +0530

    chore: Remove hederaClient and index files, update package.json for module support and converted to ts

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ import { HumanMessage } from "@langchain/core/messages";
 import { HederaAgentKit, createHederaTools } from "hedera-agent-kit";
 import { ChatOpenAI } from "@langchain/openai";
 import * as dotenv from "dotenv";
-import { createInstance } from "./hederaClient.js";
 
 // Load environment variables
 dotenv.config();
@@ -18,13 +17,11 @@ const llm = new ChatOpenAI({
   maxTokens: 1000,
 });
 
-// const hederaClient = createInstance({});
-
 // Initialize Hedera client with account ID and private key from environment variables
 const hederaAgentKit = new HederaAgentKit(
-  process.env.HEDERA_ACCOUNT_ID,
-  process.env.HEDERA_ACCOUNT_PRIVATE_KEY,
-  process.env.HEDERA_ACCOUNT_PUBLIC_KEY,
+  process.env.HEDERA_ACCOUNT_ID!,
+  process.env.HEDERA_ACCOUNT_PRIVATE_KEY!,
+  process.env.HEDERA_ACCOUNT_PUBLIC_KEY!,
   "testnet"
 );
 const hederaAgentKitTools = createHederaTools(hederaAgentKit);
@@ -44,18 +41,12 @@ const rlp = readline.createInterface({
   output: process.stdout,
 });
 
-async function readUserPrompt() {
-  const lines = [];
-  while (true) {
-    const line = await rlp.question("");
-    if (line === "" && lines[lines.length - 1] === "") {
-      return lines.join("\n");
-    }
-    lines.push(line);
-  }
+async function readUserPrompt(): Promise<string> {
+  const line = await rlp.question("");
+  return line;
 }
 
-async function obtainAgentReply(userPrompt) {
+async function obtainAgentReply(userPrompt: string): Promise<string> {
   const reply = await agent.invoke(
     {
       messages: [new HumanMessage(userPrompt)],
@@ -73,7 +64,7 @@ async function obtainAgentReply(userPrompt) {
   return agentReply;
 }
 
-async function main() {
+async function main(): Promise<void> {
   while (true) {
     console.log("You:\n");
     const userPrompt = await readUserPrompt();
